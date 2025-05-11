551379022ed49746c03ff1be671f7cdfe09d69fb
ErinaMustafa <erina.mustafa@student.uni-pr.edu>
2025-05-12 00:00:11 +0200

    Update

--- cookies_contact.php
+++ cookies_contact.php
@@ -7,14 +7,20 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $subject = trim($_POST["subject"]);
     $message = trim($_POST["message"]);
 
-    // Vendosim cookies për 30 ditë
-    setcookie("user_name", $name, time() + (30 * 24 * 60 * 60), "/"); // 30 ditë
-    setcookie("user_email", $email, time() + (30 * 24 * 60 * 60), "/");
-    setcookie("user_subject", $subject, time() + (30 * 24 * 60 * 60), "/");
-    setcookie("user_message", $message, time() + (30 * 24 * 60 * 60), "/");
+    // Ruajmë të dhënat në një array dhe i kodifikojmë në JSON
+    $user_data = array(
+        'name' => $name,
+        'email' => $email,
+        'subject' => $subject,
+        'message' => $message
+    );
+    
+    // Kodifikojmë të dhënat në JSON dhe i vendosim në cookie
+    setcookie("user_data", json_encode($user_data), time() + (30 * 24 * 60 * 60), "/");
 
     // Mund ta dërgojmë mesazhin që dërguam me sukses:
     header("Location: thanks_contact.php"); // Ridrejtojmë përdoruesin pas dërgimit
     exit();
 }
 ?>
+
