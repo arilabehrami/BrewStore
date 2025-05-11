982f5928c0fe4d3ccb89b055593127ca83540d31
arila22 <arila.behrami@student.uni-pr.edu>
2025-05-11 17:32:44 +0200

    Update index.php

--- pages/index.php
+++ pages/index.php
@@ -108,18 +108,18 @@
                 <div class="footer-col">
                     <h4>Company</h4>
                     <ul>
-                        <li><a href="about.php">About Us</a></li>
-                        <li><a href="products.php">Our Services</a></li>
-                        <li><a href="contact.php">Privacy Policy</a></li>
-                        <li><a href="order.php">Order Now</a></li>
+                        <li><a href="pages/about.php">About Us</a></li>
+                        <li><a href="pages/products.php">Our Services</a></li>
+                        <li><a href="pages/contact.php">Privacy Policy</a></li>
+                        <li><a href="pages/order.php">Order Now</a></li>
                     </ul>
                 </div>
                 <div class="footer-col">
                     <h4>Get help</h4>
                     <ul>
                         <li><a href="#">FAQ</a></li>
-                        <li><a href="products.php" target="_blank">Shipping</a></li>
-                        <li><a href="contact.php" target="_blank">Returns</a></li>
+                        <li><a href="pages/products.php" target="_blank" >Shipping</a></li>
+                        <li><a href="pages/contact.php" target="_blank">Returns</a></li>
                         <li><a href="#">Payment Options</a></li>
                     </ul>
                 </div>
@@ -133,6 +133,7 @@
                         <li><a href="https://www.youtube.com/" aria-label="Youtube"><i class='bx bxl-youtube'></i></a></li>
                     </ul>
                     </div>
+                </div>
             </div>
         </div>
     </footer>
