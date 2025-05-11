b2c14a8529ada9d79fee4ee3774f7b6b6858a119
arila22 <arila.behrami@student.uni-pr.edu>
2025-05-11 19:18:26 +0200

    Update footer.php

---
+++ includes/footer.php
@@ -0,0 +1,46 @@
+
+    
+    <footer class="footer">
+        <div class="container">
+            <div class="row">
+                <div class="footer-col">
+                    <h4>Company</h4>
+                    <ul>
+                        <li><a href="about.php">About Us</a></li>
+                        <li><a href="products.php">Our Services</a></li>
+                        <li><a href="contact.php">Privacy Policy</a></li>
+                        <li><a href="order.php">Order Now</a></li>
+                    </ul>
+                </div>
+                <div class="footer-col">
+                    <h4>Get help</h4>
+                    <ul>
+                        <li><a href="#">FAQ</a></li>
+                        <li><a href="products.php" target="_blank">Shipping</a></li>
+                        <li><a href="contact.php" target="_blank">Returns</a></li>
+                        <li><a href="#">Payment Options</a></li>
+                    </ul>
+                </div>
+                <div class="footer-col">
+                    <h4>Follow us</h4>
+                    <div class="social-links">
+                    <ul>
+                        <li><a href="https://www.facebook.com/" aria-label="Facebook"><i class='bx bxl-facebook'></i></a></li>
+                        <li><a href="https://www.instagram.com/" aria-label="Instagram"><i class='bx bxl-instagram'></i></a></li>
+                        <li><a href="https://www.twitter.com/" aria-label="Twitter"><i class='bx bxl-twitter'></i></a></li>
+                        <li><a href="https://www.youtube.com/" aria-label="Youtube"><i class='bx bxl-youtube'></i></a></li>
+                    </ul>
+                    </div>
+            </div>
+        </div>
+    </footer>
+    <div id="warningMessage" style="color: red;"></div>
+    <button onclick="shfaqVerejtje()">Shfaq Vërejtjen</button>
+
+
+</body>
+</html>
+
+
+
+
