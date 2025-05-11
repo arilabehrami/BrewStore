1291155986e64933fe4c559666aa278ab66faee9
arila22 <arila.behrami@student.uni-pr.edu>
2025-05-11 17:32:41 +0200

    Update contact.php

--- pages/contact.php
+++ pages/contact.php
@@ -189,24 +189,24 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     </section>
 
     
-    <footer class="footer">
+   <footer class="footer">
         <div class="container">
             <div class="row">
                 <div class="footer-col">
                     <h4>Company</h4>
                     <ul>
-                        <li><a href="about.php">About Us</a></li>
-                        <li><a href="products.php">Our Services</a></li>
-                        <li><a href="contact.php">Privacy Policy</a></li>
-                        <li><a href="order.php">Order Now</a></li>
+                        <li><a href="pages/about.php">About Us</a></li>
+                        <li><a href="pages/products.php">Our Services</a></li>
+                        <li><a href="pages/contact.php">Privacy Policy</a></li>
+                        <li><a href="pages/order.php">Order Now</a></li>
                     </ul>
                 </div>
                 <div class="footer-col">
                     <h4>Get help</h4>
                     <ul>
                         <li><a href="#">FAQ</a></li>
-                        <li><a href="products.php" target="_blank">Shipping</a></li>
-                        <li><a href="contact.php" target="_blank">Returns</a></li>
+                        <li><a href="pages/products.php" target="_blank" >Shipping</a></li>
+                        <li><a href="pages/contact.php" target="_blank">Returns</a></li>
                         <li><a href="#">Payment Options</a></li>
                     </ul>
                 </div>
@@ -219,6 +219,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                         <li><a href="https://www.twitter.com/" aria-label="Twitter"><i class='bx bxl-twitter'></i></a></li>
                         <li><a href="https://www.youtube.com/" aria-label="Youtube"><i class='bx bxl-youtube'></i></a></li>
                     </ul>
+                    </div>
                 </div>
             </div>
         </div>
