b591c29d111bbe8ff7231357543896efcf93e416
AlbiKallaba <albi.kallaba@student.uni-pr.edu>
2025-05-12 16:39:44 +0200

    permisim i vogel(vendosja e "Shop Now" ne qender

--- index.php
+++ index.php
@@ -9,8 +9,10 @@
 
 <section class="home" id="home">
     <div class="home-text">
-       <h1><i>Start your day<br>with coffee</i></h1>
-       <a href="products.php" class="btn">Shop Now</a> 
+       <h1><i>Start your day with coffee</i></h1>
+       <div style="display: flex; justify-content: center;"> <!-- Vetëm kjo rresht është shtuar -->
+           <a href="products.php" class="btn">Shop Now</a>
+       </div>
     </div>
     <div class="home-video">
         <video autoplay loop muted src="assets/images/coffee.mp4"></video>
