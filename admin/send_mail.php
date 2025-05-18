765e3ac5cb1702b72b07917d37ced0ff665c1813
AlbiKallaba <albi.kallaba@student.uni-pr.edu>
2025-05-18 12:17:48 +0200

    .

--- admin/send_mail.php
+++ admin/send_mail.php
@@ -28,7 +28,7 @@ try {
         : 'No message';
 
     $mail->send();
-    echo '<div style="text-align: center; color: green; font-weight: bold; margin-top: 20px;">Emaili u dërgua me sukses.</div>';
+    echo '<div style="text-align: center; color: green; font-weight: bold; margin-top: 20px;">Email was sent successfully.</div>';
 } catch (Exception $e) {
     echo "Dështoi dërgimi i emailit. Error: {$mail->ErrorInfo}";
 }
